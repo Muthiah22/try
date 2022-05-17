a1990589ee3a97955ce1e2dd964554f0e35f62b7
Muthiah22 <95869918+Muthiah22@users.noreply.github.com>
2022-05-17 20:58:09 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+st.write('⭕ Don’t smoke or use tobacco')
+st.write('⭕ Manage high cholesterol, high blood pressure and diabetes')
+st.write('⭕ Eat a heart-healthy diet.')
+ 
+st.write ('⭕ Limit alcohol use')
+st.write('⭕ Manage stress')
+st.write('⭕ Increase your activity level. Exercise helps you lose weight, improve your physical condition and relieve stress.')
+st.write('⭕ Do 30 minutes of walking 5 times per week or walking 10,000 steps per day')
+ 
+new_title = '<p style="font-family:Georgia; color:#0A2E4D; font-size: 34px;">SOME FACTS ABOUT THE HEART</p>'
+st.markdown(new_title, unsafe_allow_html=True)
+st.write('⭕ The average heart is the size of a fist in an adult')
+st.write('⭕ Your heart will beat about 115,000 times each day')
+st.write('⭕ Your heart pumps about 2,000 gallons of blood every day')
+st.write('⭕ The heart pumps blood through 60,000 miles of blood vessels')
+st.write('⭕ A woman’s heart beats slightly faster than a man’s heart')
+st.write ('⭕ The heart can continue beating even when it’s disconnected from the body')
+
+st.write('⭕ Laughing is good for your heart. It reduces stress and gives a boost to your immune system')
+ 
